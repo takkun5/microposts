9d8a643164b5be935d5618bc2c35dc61c45dfd4a
takkun5 <takuya0215sai@gmail.com>
2015-07-27 14:20:20 +0000

    first commit

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -22,6 +22,6 @@ class MicropostsController < ApplicationController
   
   private
   def micropost_params
-    params.require(:micropost).permit(:content, :avatar)
+    params.require(:micropost).permit(:content,  :avatar, :avatar_cache)
   end
 end
